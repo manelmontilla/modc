b76255aa4dcaecf720a14a429bb353a44e3de6af
Manel Montilla <manel.montilla@gmail.com>
2023-02-25 16:03:00 +0100

    Version 0.0.2

--- modc.go
+++ modc.go
@@ -2,7 +2,8 @@ package modc
 
 import "runtime/debug"
 
-// CallMeMaybe It's hard to look right at you baby, but here's my number
+// CallMeMaybe It's hard to look right at you baby, but here's my number, so
+// call me, maybe?
 func CallMeMaybe() string {
 	v, _ := debug.ReadBuildInfo()
 	return v.Main.Version
