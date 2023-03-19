fbb09716a79e1550f3ec4c8b7bd2c4ec25722ea9
Manel Montilla <manel.montilla@gmail.com>
2023-03-19 12:17:16 +0100

    Version 1.0.1

--- modc.go
+++ modc.go
@@ -6,7 +6,7 @@ import (
 
 const modName = "github.com/manelmontilla/modc"
 
-// CallMeMaybe Hey, I just met you, and this is crazy.
+// CallMeMaybe Hey, I just met you, and this is crazy.But here's my number
 func CallMeMaybe() string {
 	v, _ := debug.ReadBuildInfo()
 	version := "unknown"
